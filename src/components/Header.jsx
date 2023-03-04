1771102a678b2a8ae8a6b813454516148c18ea47
theshrish46 <theshrish46@gmail.com>
2023-03-04 21:39:51 +0530

    on 4-3-23

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav>
+      <h1>TechStar</h1>
+
+      <main>
+
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+        <Link to={"/brands"}>Brands</Link>
+        <Link to={"/service"}>Services</Link>
+      </main>
+    </nav>
+  )
+}
+
+export default Header
